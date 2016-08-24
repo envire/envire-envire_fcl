e2c20bb86d3d9e144aabf42841d9851f4e26a785
Christoph Hertzberg <chtz@informatik.uni-bremen.de>
2016-08-24 11:09:44 +0200

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace envire_fcl
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace envire_fcl
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <envire_fcl/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    envire_fcl::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
