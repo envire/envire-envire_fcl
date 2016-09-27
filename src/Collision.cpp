6e2e535d5aeb9d1686814eeb1877ef752cb97f84
Christoph Hertzberg <chtz@informatik.uni-bremen.de>
2016-09-27 15:36:48 +0200

    Interactive collision of sphere with MLSMapSloped using EnvireVisualizer

--- src/Collision.cpp
+++ src/Collision.cpp
@@ -1,1 +1,14 @@
 #include "Collision.hpp"
+
+
+namespace fcl {
+
+template void collide_mls(
+        const maps::grid::MLSMapSloped& mls,
+        const Transform3<float>& tf2,
+        const fcl::Spheref* o2,
+        const CollisionRequest<float>& request,
+        CollisionResult<float>& result
+);
+
+}  // namespace fcl
--- src/fcl-test.cpp
+++ src/fcl-test.cpp
@@ -1,78 +1,49 @@
 //#define EIGEN_INITIALIZE_MATRICES_BY_NAN 1
 
-#include "fcl-extern.hpp"
+#include "Collision.hpp"
 
 #include <iostream>
 #include <boost/archive/polymorphic_binary_iarchive.hpp>
+
+#include <envire_core/items/Item.hpp>
+#include <envire_visualizer/EnvireVisualizerWindow.hpp>
+#include <envire_core/graph/EnvireGraph.hpp>
+#include <envire_core/items/Transform.hpp>
+
 #include <vizkit3d/MLSMapVisualization.hpp>
 #include <vizkit3d/Vizkit3DWidget.hpp>
 #include <vizkit3d/QtThreadedWidget.hpp>
+#include <plugin_manager/PluginLoader.hpp>
 
 #include <Eigen/Geometry>
+using namespace envire::viz;
 
 
 
-typedef std::shared_ptr<fcl::CollisionGeometry<float> > GeoPtr;
-typedef maps::grid::MLSMapSloped MLSMapS;
 
-namespace fcl {
-
-template<class Shape, class S>
-void collide_mls(
-        const maps::grid::MLSMapSloped& mls,
-        const Transform3<S>& tf2,
-        const Shape* o2,
-        const CollisionRequest<S>& request,
-        CollisionResult<S>& result
-)
+struct EdgeCallBack : public envire::core::GraphEventDispatcher
 {
-    // FIXME Local frame of MLS is slightly annoying here ...
-    const Transform3<S> world2map = mls.getLocalFrame().cast<S>();
-    const Transform3<S> shape2map = world2map * tf2;
-
-    // compute bounding volume of o2 relative to mls map
-    AABB<S> bv2;
-    computeBV(*o2, shape2map, bv2);
-    std::cout << "world2map:\n" << world2map.matrix() << "\nshape2map:\n" << shape2map.matrix() << "\nBV: [" << bv2.min_.transpose() << "] - [" << bv2.max_.transpose() << "]\n";
-
-    typedef fcl::AABB<S> BV;
-    typedef maps::grid::MLSMapSloped::Patch P;
-
-    typedef std::vector<fcl::Triangle> TriVect;
-    static fcl::Triangle triags[4];
-    for(int i=0; i<4; ++i) triags[i].set(0, i+1, i+2);
-    fcl::BVHModel<BV> m1;  // BVHModel of local MLS region
-    // m1.bv_splitter.reset(new fcl::detail::BVSplitter<BV>(fcl::detail::SPLIT_METHOD_MEDIAN);
-    //estimate number of cells:
-    size_t num_cells = ((bv2.max_ - bv2.min_).template head<2>().cwiseQuotient(mls.getResolution().cast<S>())).prod();
-    Eigen::Vector2f cell_size = mls.getResolution().cast<float>();
-    std::cout << "num_cells: " << num_cells << ", cell_size: " << cell_size.transpose() << "\n";
-    m1.beginModel(2*num_cells, 4*num_cells); // assume that each cell creates one 4-gon on average
-    Eigen::AlignedBox3d bounding(bv2.min_.template cast<double>(), bv2.max_.template cast<double>());
-    mls.intersectAABB_callback(bounding,
-            [&m1, &cell_size](maps::grid::Index idx, const P& p) {
-//        std::cout << "Intersect at " << idx.transpose() << " with p from " << p.getMin() << " to " << p.getMax() << "\t";
-        std::vector<Eigen::Vector3f> points;
-        maps::grid::getPolygon(points, p, idx, cell_size);
-//        std::cout << "Polygon:";
-//        for(auto pt : points) std::cout << " [" << pt.transpose() << "]";
-//        std::cout << std::endl;
-//        if(points.size() < 3) return;
-//        if(points.size() > 6) std::cout << "WHAT? " << p.getNormal().transpose() << "\n\n";
-        assert(points.size()>=3 && points.size() <=6);
-        m1.addSubModel(points, TriVect(triags, triags+(points.size()-2)));
-    });
-    if(m1.num_vertices==0)
-    {
-        // no contacts at all, m1.endModel() would fail, so we directly return here
-        return;
+    const envire::core::EnvireGraph &graph_;
+    const MLSMapS& mls_;
+public:
+    EdgeCallBack(const envire::core::EnvireGraph& graph, const MLSMapS & mls) : graph_(graph), mls_(mls) {}
+    void edgeModified(const envire::core::EdgeModifiedEvent& e) {
+        std::cout << "Edge modified. Trafo=\n";
+        std::cout << graph_.getEdgeProperty(e.edge).transform.getTransform().matrix();
+        fcl::Transform3f trafo = graph_.getEdgeProperty(e.edge).transform.getTransform().cast<float>();
+        fcl::CollisionRequestf request(10, true, 10, true);
+        fcl::CollisionResultf result;
+        fcl::Spheref sphere(.25);
+        fcl::collide_mls(mls_, trafo, &sphere, request, result);
+        std::cout << "\nisCollision()==" << result.isCollision();
+        for(size_t i=0; i< result.numContacts(); ++i)
+        {
+            const auto & cont = result.getContact(i);
+            std::cout << "\n" << cont.pos.transpose() << "; " << cont.normal.transpose() << "; " << cont.penetration_depth;
+        }
+        std::cout << std::endl;
     }
-    m1.endModel();
-    fcl::collide(&m1, world2map.inverse(Eigen::Isometry), o2, tf2, request, result);
-}
-
-
-}  // namespace fcl
+};
 
 
 int main(int argc, char **argv) {
@@ -82,15 +53,27 @@ int main(int argc, char **argv) {
         std::cout << "Usage:\n" << argv[0] << " mlsFileName\n";
         return 0;
     }
+    plugin_manager::PluginLoader* loader = plugin_manager::PluginLoader::getInstance();
+    envire::core::ItemBase::Ptr item;
+    loader->createInstance("envire::core::Item<maps::grid::MLSMapSloped>", item);
+
+    envire::core::Item<MLSMapS>::Ptr mlsItem = boost::dynamic_pointer_cast<envire::core::Item<MLSMapS>>(item);
+
+
 
     std::ifstream fileIn(argv[1]);
 
+
+
     // deserialize from file stream
     boost::archive::binary_iarchive mlsIn(fileIn);
-    std::shared_ptr<MLSMapS> mlsSloped(new MLSMapS);
+//    std::shared_ptr<MLSMapS> mlsSloped(new MLSMapS);
+//    envire::core::Item<MLSMapS>::Ptr mlsItem(new envire::core::Item<MLSMapS>());
 
-    mlsIn >> *mlsSloped;
-    std::shared_ptr<fcl::Spheref> geo1(new fcl::Spheref(.251));
+    mlsIn >> mlsItem->getData();
+//    mlsIn >> *mlsSloped;
+
+    std::shared_ptr<fcl::Spheref> geo1(new fcl::Spheref(.25));
 
     fcl::CollisionRequestf request(10, true, 10, true);
     fcl::CollisionResultf result;
@@ -102,7 +85,7 @@ int main(int argc, char **argv) {
     {
         result.clear(); // Must clear result before calling fcl::collide functions!!!
         trafo.translation().z() = z;
-        fcl::collide_mls(*mlsSloped, trafo, geo1.get(), request, result);
+        fcl::collide_mls(mlsItem->getData(), trafo, geo1.get(), request, result);
         //    fcl::collide(&o1, &o2, request, result);
         std::cout << "With z = " << z << ", isCollision()==" << result.isCollision();
         for(size_t i=0; i< result.numContacts(); ++i)
@@ -115,9 +98,35 @@ int main(int argc, char **argv) {
     }
 
 
+    QApplication app(argc, argv);
+    EnvireVisualizerWindow window;
+    std::shared_ptr<envire::core::EnvireGraph> graph(new envire::core::EnvireGraph);
+    graph->addFrame("A");
+    graph->addFrame("B");
+    graph->addItemToFrame("A", mlsItem);
+    envire::core::Transform ab(base::Position(1, 1, 1), Eigen::Quaterniond::Identity());
+    graph->addTransform("A", "B", ab);
+    window.displayGraph(graph, "A");
+    window.show();
+
+    graph->subscribe(new EdgeCallBack(*graph, mlsItem->getData()));
+
+//    std::thread t([&graph](){
+//          while(true)
+//          {
+//          std::this_thread::sleep_for(std::chrono::milliseconds(300));
+//          envire::core::Transform ab = graph->getTransform("A", "B");
+////          ab.transform.translation.x() += 0.1;
+////          std::cout << ab.transform.translation.x() << std::endl;
+////          graph->updateTransform("A", "B", ab);
+//          }
+//      });
+
+    app.exec();
 
 
 
+#if 0
     QtThreadedWidget<vizkit3d::Vizkit3DWidget> app;
     app.start();
     //QApplication app(argc, argv);
@@ -140,5 +149,5 @@ int main(int argc, char **argv) {
         //
     }
 
-
+#endif
 }
