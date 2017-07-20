0dfd422b1ff7d63d8417c03ab6f3869624bdeeb2
Raul Dominguez <raul.dominguez@dfki.de>
2017-07-20 19:16:09 +0200

    Working on a test of multiple collision objects of different types

---
+++ src/fcl-collidable-test-multiple-objects.cpp
@@ -0,0 +1,178 @@
+/*
+ * fcl-collidable-test-multiple-objects.cpp
+ * Copyright (C) 2017 rdominguez <rdominguez@RDOMINGUEZ>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <QObject>
+#include "Collision.hpp"
+
+#include <iostream>
+#include <boost/archive/polymorphic_binary_iarchive.hpp>
+
+#include <smurf/Robot.hpp>
+
+#include <envire_core/items/Item.hpp>
+#include <envire_visualizer/EnvireVisualizerWindow.hpp>
+#include <envire_core/graph/EnvireGraph.hpp>
+#include <envire_core/items/Transform.hpp>
+
+#include <envire_smurf/GraphLoader.hpp>
+
+#include <vizkit3d/Vizkit3DWidget.hpp>
+#include <vizkit3d/QtThreadedWidget.hpp>
+#include <plugin_manager/PluginLoader.hpp>
+
+#include <envire_core/graph/GraphViz.hpp>
+
+#include <Eigen/Geometry>
+
+#define CENTER_FRAME_NAME std::string("center")
+#define MLS_FRAME_NAME std::string("MLS Frame")
+
+using namespace envire::viz;
+using CollisionType = smurf::Collidable;
+using CollisionItem = envire::core::Item<CollisionType>;
+using IterCollItem = envire::core::EnvireGraph::ItemIterator<CollisionItem>;
+
+struct EdgeCallBack : public envire::core::GraphEventDispatcher
+{
+    const envire::core::EnvireGraph &graph_;
+    const envire::core::FrameId & colFrame_;
+    const MLSMapS& mls_;
+
+public:
+    EdgeCallBack(const envire::core::EnvireGraph& graph, envire::core::FrameId colFrame, const MLSMapS & mls) : graph_(graph), colFrame_(colFrame), mls_(mls) {
+    }
+    void edgeModified(const envire::core::EdgeModifiedEvent& e) {
+
+        smurf::Collidable collidable;
+        IterCollItem itCols, endCols;
+        itCols = graph_.getItem<CollisionItem>(colFrame_); 
+        collidable = itCols->getData();
+        // Transformation must be from the mls frame to the colision object frame
+        envire::core::Transform tfColCen = graph_.getTransform(MLS_FRAME_NAME, colFrame_);
+        fcl::Transform3f trafo = tfColCen.transform.getTransform().cast<float>();
+        std::cout << "Edge modified. Trafo=\n" << std::endl;
+        std::cout << tfColCen.transform.getTransform().matrix() << std::endl;
+        // TODO Here I must give the transformation between the object and the mls frame, currently is only taking the transformation of the edge that just changed. For doing this, you could pass instead of the smurf::Collidable, the frame where it is located, here get the collidable and the correct transformation
+        //fcl::Transform3f trafo = graph_.getEdgeProperty(e.edge).transform.getTransform().cast<float>();
+        
+        fcl::CollisionRequestf request(10, true, 10, true);
+        fcl::CollisionResultf result;
+        urdf::Collision collision = collidable.getCollision();
+
+        if (collision.geometry->type == urdf::Geometry::SPHERE)
+        {
+            boost::shared_ptr<urdf::Sphere> sphereUrdf = boost::dynamic_pointer_cast<urdf::Sphere>(collision.geometry);
+            fcl::Spheref sphere(sphereUrdf->radius);
+
+            fcl::collide_mls(mls_, trafo, &sphere, request, result);
+        }
+        else
+        {
+          // Box
+          boost::shared_ptr<urdf::Box> boxUrdf = boost::dynamic_pointer_cast<urdf::Box>(collision.geometry);
+          fcl::Boxf box(boxUrdf->dim.x, boxUrdf->dim.y, boxUrdf->dim.z);
+          fcl::collide_mls(mls_, trafo, &box, request, result);
+        }
+
+        std::cout << "\nisCollision()==" << result.isCollision();
+        for(size_t i=0; i< result.numContacts(); ++i)
+        {
+            const auto & cont = result.getContact(i);
+            std::cout << "\n" << cont.pos.transpose() << "; " << cont.normal.transpose() << "; " << cont.penetration_depth;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main(int argc, char **argv) {
+
+    // Use smurf_loader to load an smurf file
+    const std::string path="./smurf/multiple_objects/smurf/multiple_objects.smurf";
+    smurf::Robot* robot = new(smurf::Robot);
+    robot->loadFromSmurf(path);
+    envire::core::GraphViz viz;
+    envire::core::Transform iniPose;
+
+    std::shared_ptr<envire::core::EnvireGraph> graph(new envire::core::EnvireGraph) ;
+    iniPose.transform.orientation = base::Quaterniond::Identity();
+    iniPose.transform.translation << 1.0, 1.0, 1.0;
+    envire::smurf::GraphLoader graphLoader(graph, iniPose);
+    envire::core::FrameId center = CENTER_FRAME_NAME;
+    graph->addFrame(center);
+    graphLoader.loadStructure(graph->getVertex(center), *robot);
+    
+    int nextGroupId = 0;
+    graphLoader.loadFrames(nextGroupId, *robot);
+    graphLoader.loadCollidables(*robot);
+    viz.write(*(graphLoader.getGraph()), "fcl-collidable-test-multiple-.dot");
+    
+    // Load the mls
+    if(argc < 3)
+    {
+        std::cout << "Usage:\n" << argv[0] << " mlsFileName " << "sloped|kalman \n";
+        return 0;
+    }
+
+    plugin_manager::PluginLoader* loader = plugin_manager::PluginLoader::getInstance();
+    envire::core::ItemBase::Ptr item;
+    std::string mapType = argv[2];
+    if (mapType == "sloped")
+    {
+        loader->createInstance("envire::core::Item<maps::grid::MLSMapSloped>", item);
+        envire::core::Item<MLSMapS>::Ptr mlsItem = boost::dynamic_pointer_cast<envire::core::Item<MLSMapS>>(item);
+        std::ifstream fileIn(argv[1]);
+        // deserialize from file stream
+        boost::archive::binary_iarchive mlsIn(fileIn);
+        mlsIn >> mlsItem->getData();
+        QApplication app(argc, argv);
+        EnvireVisualizerWindow window;
+        graph->addFrame(MLS_FRAME_NAME);
+        graph->addItemToFrame(MLS_FRAME_NAME, mlsItem);
+        envire::core::Transform mlsCenter(base::Position(0.0, 0.0, 0.0), Eigen::Quaterniond::Identity());
+        graph->addTransform(MLS_FRAME_NAME, CENTER_FRAME_NAME, mlsCenter);
+        QString qCentreName = QString::fromStdString(CENTER_FRAME_NAME);
+        window.displayGraph(graph, qCentreName);
+        window.show();
+        // Create the callback to compute collisions in those frames which have any collidable:
+        // Find out the frames which contain a collidable
+        //
+        
+        envire::core::EnvireGraph::vertex_iterator  it, end;
+        std::tie(it, end) = graph->getVertices();
+        for(; it != end; ++it)
+        {
+            // See if the vertex has collision objects
+            IterCollItem itCols, endCols;
+            std::tie(itCols, endCols) = graph->getItems<CollisionItem>(*it);
+
+            if(itCols != endCols)
+            {
+                //allCollVertices.push_back(it->getData());
+                envire::core::FrameId colFrame = graph->getFrameId(*it);
+                std::cout << "Collision items found in frame " << colFrame << std::endl;
+                graph->subscribe(new EdgeCallBack(*graph, colFrame, mlsItem->getData()));
+            }
+        }
+        /*
+        envire::core::FrameId colFrame = SMURF_COLLIDABLE_SPHERE_FRAME_NAME;
+        IterCollItem itCols, endCols;
+        std::tie(itCols, endCols) = graph->getItems<CollisionItem>(colFrame);
+        // TODO This you have to do for all the frames not only for one, but by now just pass the frame instead of the collidable to the EdgeCallBack method and move this retrieval of the collidables there
+        if(itCols != endCols)
+        {
+            //Before: graph->subscribe(new EdgeCallBack(*graph, itCols->getData(), mlsItem->getData()));
+            graph->subscribe(new EdgeCallBack(*graph, colFrame, mlsItem->getData()));
+        }
+        */
+
+        app.exec();
+    }
+    else
+    {
+        std::cout << "Only deserialization of sloped mls is supported";
+    }
+}
