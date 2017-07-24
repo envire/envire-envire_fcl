7b69dd4a2dbfe09c25ee06a3ad2494d221fa0130
Raul Dominguez <raul.dominguez@dfki.de>
2017-07-24 13:48:25 +0200

    Test for collisions with multiple types of objects is working

--- src/fcl-collidable-test-multiple-objects.cpp
+++ src/fcl-collidable-test-multiple-objects.cpp
@@ -39,52 +39,66 @@ using IterCollItem = envire::core::EnvireGraph::ItemIterator<CollisionItem>;
 struct EdgeCallBack : public envire::core::GraphEventDispatcher
 {
     const envire::core::EnvireGraph &graph_;
-    const envire::core::FrameId & colFrame_;
+    const std::vector<envire::core::FrameId> &colFrames_;
     const MLSMapS& mls_;
 
 public:
-    EdgeCallBack(const envire::core::EnvireGraph& graph, envire::core::FrameId colFrame, const MLSMapS & mls) : graph_(graph), colFrame_(colFrame), mls_(mls) {
+    EdgeCallBack(const envire::core::EnvireGraph& graph, std::vector<envire::core::FrameId> &colFrames, const MLSMapS & mls) : graph_(graph), colFrames_(colFrames), mls_(mls) {
     }
     void edgeModified(const envire::core::EdgeModifiedEvent& e) {
-
+        /*
+         * Each time a edge is modified, this method iterates over the frames
+         * which have a collision object and computes whether collision with
+         * the MLS takes place or not.
+         */
         smurf::Collidable collidable;
         IterCollItem itCols, endCols;
-        itCols = graph_.getItem<CollisionItem>(colFrame_); 
-        collidable = itCols->getData();
-        // Transformation must be from the mls frame to the colision object frame
-        envire::core::Transform tfColCen = graph_.getTransform(MLS_FRAME_NAME, colFrame_);
-        fcl::Transform3f trafo = tfColCen.transform.getTransform().cast<float>();
-        std::cout << "Edge modified. Trafo=\n" << std::endl;
-        std::cout << tfColCen.transform.getTransform().matrix() << std::endl;
-        // TODO Here I must give the transformation between the object and the mls frame, currently is only taking the transformation of the edge that just changed. For doing this, you could pass instead of the smurf::Collidable, the frame where it is located, here get the collidable and the correct transformation
-        //fcl::Transform3f trafo = graph_.getEdgeProperty(e.edge).transform.getTransform().cast<float>();
-        
-        fcl::CollisionRequestf request(10, true, 10, true);
-        fcl::CollisionResultf result;
-        urdf::Collision collision = collidable.getCollision();
-
-        if (collision.geometry->type == urdf::Geometry::SPHERE)
+        for(unsigned int frameIndex = 0; frameIndex<colFrames_.size(); ++frameIndex)
         {
-            boost::shared_ptr<urdf::Sphere> sphereUrdf = boost::dynamic_pointer_cast<urdf::Sphere>(collision.geometry);
-            fcl::Spheref sphere(sphereUrdf->radius);
-
-            fcl::collide_mls(mls_, trafo, &sphere, request, result);
+            itCols = graph_.getItem<CollisionItem>(colFrames_[frameIndex]); 
+            std::cout << "Collision related to frame " << colFrames_[frameIndex] << std::endl;
+            collidable = itCols->getData();
+            // Transformation must be from the mls frame to the colision object frame
+            envire::core::Transform tfColCen = graph_.getTransform(MLS_FRAME_NAME, colFrames_[frameIndex]);
+            fcl::Transform3f trafo = tfColCen.transform.getTransform().cast<float>();
+            std::cout << "Edge modified. Trafo=\n" << std::endl;
+            std::cout << tfColCen.transform.getTransform().matrix() << std::endl;
+            fcl::CollisionRequestf request(10, true, 10, true);
+            fcl::CollisionResultf result;
+            urdf::Collision collision = collidable.getCollision();
+            bool collisionComputed = true;
+            switch (collision.geometry->type){
+                case urdf::Geometry::SPHERE:
+                    {
+                        std::cout << "Collision with a sphere" << std::endl;
+                        boost::shared_ptr<urdf::Sphere> sphereUrdf = boost::dynamic_pointer_cast<urdf::Sphere>(collision.geometry);
+                        fcl::Spheref sphere(sphereUrdf->radius);
+                        fcl::collide_mls(mls_, trafo, &sphere, request, result);
+                        break;
+                    }
+                case urdf::Geometry::BOX:
+                    {
+                        std::cout << "Collision with a box" << std::endl;
+                        boost::shared_ptr<urdf::Box> boxUrdf = boost::dynamic_pointer_cast<urdf::Box>(collision.geometry);
+                        fcl::Boxf box(boxUrdf->dim.x, boxUrdf->dim.y, boxUrdf->dim.z);
+                        fcl::collide_mls(mls_, trafo, &box, request, result);
+                        break;
+                    }
+                default:
+                    std::cout << "Collision with the selected geometry type not implemented" << std::endl;
+                    collisionComputed = false;
+            }
+            if (collisionComputed)
+            {
+                std::cout << "\nisCollision()==" << result.isCollision();
+                for(size_t i=0; i< result.numContacts(); ++i)
+                {
+                    const auto & cont = result.getContact(i);
+                    std::cout << "\n" << cont.pos.transpose() << "; " << cont.normal.transpose() << "; " << cont.penetration_depth;
+                }
+            std::cout << std::endl;
+            }
         }
-        else
-        {
-          // Box
-          boost::shared_ptr<urdf::Box> boxUrdf = boost::dynamic_pointer_cast<urdf::Box>(collision.geometry);
-          fcl::Boxf box(boxUrdf->dim.x, boxUrdf->dim.y, boxUrdf->dim.z);
-          fcl::collide_mls(mls_, trafo, &box, request, result);
-        }
-
-        std::cout << "\nisCollision()==" << result.isCollision();
-        for(size_t i=0; i< result.numContacts(); ++i)
-        {
-            const auto & cont = result.getContact(i);
-            std::cout << "\n" << cont.pos.transpose() << "; " << cont.normal.transpose() << "; " << cont.penetration_depth;
-        }
-        std::cout << std::endl;
     }
 };
 
@@ -137,10 +151,8 @@ int main(int argc, char **argv) {
         QString qCentreName = QString::fromStdString(CENTER_FRAME_NAME);
         window.displayGraph(graph, qCentreName);
         window.show();
-        // Create the callback to compute collisions in those frames which have any collidable:
-        // Find out the frames which contain a collidable
-        //
-        
+        // Find out the frames which contain a collidablem put them in a vector and pass it to the callback
+        std::vector<envire::core::FrameId> colFrames;
         envire::core::EnvireGraph::vertex_iterator  it, end;
         std::tie(it, end) = graph->getVertices();
         for(; it != end; ++it)
@@ -148,27 +160,15 @@ int main(int argc, char **argv) {
             // See if the vertex has collision objects
             IterCollItem itCols, endCols;
             std::tie(itCols, endCols) = graph->getItems<CollisionItem>(*it);
-
             if(itCols != endCols)
             {
-                //allCollVertices.push_back(it->getData());
                 envire::core::FrameId colFrame = graph->getFrameId(*it);
+                colFrames.push_back(colFrame);
                 std::cout << "Collision items found in frame " << colFrame << std::endl;
-                graph->subscribe(new EdgeCallBack(*graph, colFrame, mlsItem->getData()));
             }
         }
-        /*
-        envire::core::FrameId colFrame = SMURF_COLLIDABLE_SPHERE_FRAME_NAME;
-        IterCollItem itCols, endCols;
-        std::tie(itCols, endCols) = graph->getItems<CollisionItem>(colFrame);
-        // TODO This you have to do for all the frames not only for one, but by now just pass the frame instead of the collidable to the EdgeCallBack method and move this retrieval of the collidables there
-        if(itCols != endCols)
-        {
-            //Before: graph->subscribe(new EdgeCallBack(*graph, itCols->getData(), mlsItem->getData()));
-            graph->subscribe(new EdgeCallBack(*graph, colFrame, mlsItem->getData()));
-        }
-        */
-
+        EdgeCallBack * callBack = new EdgeCallBack(*graph, colFrames, mlsItem->getData());
+        graph->subscribe(callBack);
         app.exec();
     }
     else
